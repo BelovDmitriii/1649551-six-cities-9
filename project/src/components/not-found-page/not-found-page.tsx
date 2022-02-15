107b6aac19b2f6a655bb2d298d967c0ba2a8916d
Dmitrii <belovd90@mail.ru>
2022-02-16 00:36:05 +0300

    Добавит компонент Header

--- project/src/components/not-found-page/not-found-page.tsx
+++ project/src/components/not-found-page/not-found-page.tsx
@@ -1,8 +1,42 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage(): JSX.Element {
   return(
-    <div>
-      <img  src="img/not-found.jpg" alt="not found page" width='100%' />
-    </div>
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Link className="header__logo-link" to="/">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </Link>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <Link className="header__nav-link header__nav-link--profile" to="/">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                  </Link>
+                </li>
+                <li className="header__nav-item">
+                  <Link className="header__nav-link" to="/">
+                    <span className="header__signout">Sign out</span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <div className="container">
+        <Link className="header__logo-link" to="/">
+          <h2>Back to main page</h2>
+        </Link>
+        <img src="img/not-found.jpg" alt="not found page" width='100%' />
+      </div>
+    </>
   );
 }
 
