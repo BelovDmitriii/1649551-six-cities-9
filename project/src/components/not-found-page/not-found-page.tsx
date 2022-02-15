867ea0c706917b14701ad2e422d212bb87aba864
Dmitrii <belovd90@mail.ru>
2022-02-16 00:59:25 +0300

    Обновит ссылку для Хедера

--- project/src/components/not-found-page/not-found-page.tsx
+++ project/src/components/not-found-page/not-found-page.tsx
@@ -1,37 +1,12 @@
 import { Link } from 'react-router-dom';
+import Header from '../header/header';
 
 function NotFoundPage(): JSX.Element {
   return(
     <>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link" to="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </Link>
-                </li>
-                <li className="header__nav-item">
-                  <Link className="header__nav-link" to="/">
-                    <span className="header__signout">Sign out</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
       <div className="container">
-        <Link className="header__logo-link" to="/">
+        <Link to="/">
           <h2>Back to main page</h2>
         </Link>
         <img src="img/not-found.jpg" alt="not found page" width='100%' />
