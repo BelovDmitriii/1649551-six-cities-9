539e71be6cc680e4ac5962777e7e7dc6cb5ab8eb
Dmitrii <belovd90@mail.ru>
2022-04-02 19:36:45 +0300

    вынесена логика редьюсера user

--- project/src/const.ts
+++ project/src/const.ts
@@ -125,3 +125,9 @@ export enum CardTypes {
   Favorites = 'favorites',
   Nearby = 'near-places',
 }
+
+export enum NameSpace {
+  data = 'DATA',
+  offers = 'OFFERS',
+  user = 'USER',
+}
---
+++ project/src/store/user-process/user-process.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {AutorizationStatus, NameSpace} from '../../const';
+
+const initialState = {
+  authorizationStatus: AutorizationStatus.Unknown,
+  login: '',
+};
+
+const userProcess = createSlice({
+  name: NameSpace.user,
+  initialState,
+  reducers: {
+    requireAuthorization: (state, action) => {
+      state.authorizationStatus = action.payload;
+    },
+  },
+});
+
+export const {requireAuthorization} = userProcess.actions;
