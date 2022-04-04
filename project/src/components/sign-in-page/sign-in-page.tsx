007bda171d42a7dece85465270d1e3cb1f4a31eb
Dmitrii <belovd90@mail.ru>
2022-04-05 00:12:59 +0300

    никнейм не отображается на страницу логин

--- project/src/components/sign-in-page/sign-in-page.tsx
+++ project/src/components/sign-in-page/sign-in-page.tsx
@@ -4,7 +4,7 @@ import SignInScreen from '../sign-in-screen/sign-in-screen';
 function SignInPage(): JSX.Element {
   return (
     <div className="page page--gray page--login">
-      <Header />
+      <Header isThisPageLogin/>
       <SignInScreen />
     </div>
   );
